03099501d76830edc294e3c534d660ccbf55dc3f
Ayron-Kevin <128102260+Ayron-Kevin@users.noreply.github.com>
2023-10-31 21:12:12 -0300

    Add files via upload

---
+++ adicionar.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <center>
+        <form id="form-add"
+        action="#" method="POST">
+        nome: <input type= "text"
+        nome="nome"><br>
+        CPF: <input type= "text"
+        name="cpf"><br>
+        telefone: <input type="text"
+        name="telefone"><br>
+        login: <input type="text"
+        name="login"><br>
+        senha: <input type="password" name="senha">
+        <br>
+    <input type="submit"
+    name="cadastrar" value="cadastrar">
+    </center>
+</body>
+</html>
